fb157fa81e31c71c7134391c993c092b44957119
Danilo Nascimento <danilodelete@hotmail.com>
2021-11-03 14:49:16 -0300

    fix: get ranking by resolution

--- measurement/comparing_images.js
+++ measurement/comparing_images.js
@@ -2,54 +2,44 @@ const fsPromises = require("fs").promises;
 var fs = require("fs");
 const Jimp = require("jimp");
 
-async function getFiles(diretorio, arquivos) {
-  if (!arquivos) arquivos = [];
+async function getFiles(dir, files) {
+  if (!files) files = [];
 
-  let listaDeArquivos = await fsPromises.readdir(diretorio);
-  for (let k in listaDeArquivos) {
-    let stat = await fsPromises.stat(diretorio + "/" + listaDeArquivos[k]);
-    if (stat.isDirectory())
-      await getFiles(diretorio + "/" + listaDeArquivos[k], arquivos);
-    else arquivos.push(diretorio + "/" + listaDeArquivos[k]);
+  let listFiles = await fsPromises.readdir(dir);
+  for (let k in listFiles) {
+    let stat = await fsPromises.stat(dir + "/" + listFiles[k]);
+    if (stat.isDirectory()) await getFiles(dir + "/" + listFiles[k], files);
+    else files.push(dir + "/" + listFiles[k]);
   }
 
-  return arquivos;
+  return files;
 }
 
 async function compareImage() {
-  const dataComparativeImages = [];
+  const resolutions = [];
 
   let images = await getFiles("../images");
 
   for (let i = 0; i < images.length; i++) {
-    const imageComparedTo = await Jimp.read(images[i]);
-    console.log(`====================${images[i]}====================\n`);
+    const image = await Jimp.read(images[i]);
 
-    for (let j = i; j < images.length; j++) {
-      const image = await Jimp.read(images[j]);
-
-      if (i != j) {
-        console.log(`Compare: [${images[i]}, ${images[j]}\n`);
-
-        const data = {
-          id: `${i}${j}`,
-          Comparation: `(${images[i]} | ${images[j]})`,
-          distance: Jimp.distance(imageComparedTo, image), ///distance: a distância de Hamming entre os hashes de duas imagens, ou seja. o número de bits que diferem.
-          diffInPercent: Jimp.diff(imageComparedTo, image).percent, // diff: a diferença percentual entre duas imagens.
-        };
-        dataComparativeImages.push(data);
-      }
-      continue;
-    }
+    const data = {
+      res: image.getWidth() * image.getHeight(),
+      image: images[i],
+    };
+    resolutions.push(data);
   }
+
+  resolutions.sort((a, b) => b.res - a.res);
 
   const fileName = "ranking-diff-images.json";
   const file = fs.createWriteStream(fileName);
 
-  dataComparativeImages.sort((a, b) => b.diffInPercent - a.diffInPercent);
-  const parse = JSON.stringify(dataComparativeImages);
-
-  file.write(`${parse}\n`);
+  const step = resolutions.length / 20;
+  for (let i = 0; i < resolutions.length; i += step) {
+    const parse = JSON.stringify(resolutions[Math.round(i)]);
+    file.write(`${parse}\n`);
+  }
 
   file.close();
 }
